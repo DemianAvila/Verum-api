d7c49c25e41a080fc43e60a9257a7a5330701a8f
Demian Avila <demian_avila@comunidad.unam.mx>
2022-03-05 13:03:40 -0600

    Se cambia la base de datos a mongo-db, elastic no corria en mi ram; terminado el contenedor se añade un script de javascript que permite crear la base de dtaos, añade al señor Luis como admin y crea usuarios de manipulación
    ToDo: api para manipulacion de base de datos
    
    Revert "database, shell to deploy and front end id app"
    
    This reverts commit defd27cf172c780c252aadbdb2285e1bd8737ab2.

---
+++ database/set_internal_cont.js
@@ -0,0 +1,76 @@
+//CREA LA BASE DE DATOS DE VERUM
+db = db.getSiblingDB("verum_administracion");
+//INSERTA UN DATO PARA HACER EXISTIR ESA BASE DE DATOS
+db.representantes.insert({
+    nombre: "Luis Wenceslao De Ignacio Hernández",
+    ciudad: "Ecatepec, Estado de México",
+    activo: true,
+    permisos_administrador: true,
+    fecha_modificacion: null,
+    contrasenia: null,
+    sha_code:null
+});
+//CREA DENTRO DE ESA BASE DE DATOS UN ROL DE LECTURA
+db.createRole(
+    {
+      role: "lectura", 
+      privileges: [
+        {
+          actions: [ "find" ],
+          resource: { db: "verum_administracion",
+          collection: ""}
+        }
+      ],
+      roles: []
+    }
+)
+ //CREA DENTRO DE ESA BASE DE DATOS UN ROL DE ESCRITURA
+db.createRole(
+    {
+      role: "escritura", 
+      privileges: [
+        {
+          actions: [ "insert" ],
+          resource: { db: "verum_administracion",
+          collection: ""}
+        }
+      ],
+      roles: []
+    }
+)
+//CREA DENTRO DE ESA BASE DE DATOS UN ROL DE ACTUALIZACION
+db.createRole(
+    {
+      role: "actualizacion", 
+      privileges: [
+        {
+          actions: [ "update" ],
+          resource: { db: "verum_administracion",
+          collection: ""}
+        }
+      ],
+      roles: []
+    }
+)
+//---------------------------------------------------------
+db.createUser (
+    {
+        user: "lecturas",
+        pwd: "d2UsuPRILO4n7KasPrAagESILYLZhP0J8WzWi46kUbg=",
+        roles: ["lectura"] 
+    }
+);
+db.createUser (
+    {
+        user: "escrituras",
+        pwd: "n2yYhKlvVrk4qNkvDn0MOBR1VZ8EoTzSfKrp0JlNzV0=",
+        roles: ["escritura"]
+    }
+);
+db.createUser (
+    {
+        user: "actualizaciones",
+        pwd: "tyyKhzHcff2zQwUuM+i+mS0OVvnRbXB7cO6oa4byKEA=",
+        roles: ["actualizacion"]
+    }
+);
