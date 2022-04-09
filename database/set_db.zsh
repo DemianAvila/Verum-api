ab4da3da9189a1129dd59188c6bddc06f8add2b7
Demian Avila <demian_avila@comunidad.unam.mx>
2022-04-09 00:34:58 -0500

    Espacios añadidos

--- database/set_db.zsh
+++ database/set_db.zsh
@@ -6,21 +6,21 @@ source .env
 
 #CREA Y PON EN OPERACION LA BASE DE DATOS
 docker run \
--p ${PORT_SERV}:${PORT_CONT} \
--v ${INTERNAL_STORAGE}:${EXTERNAL_STORAGE} \
---name ${MONGO_NAME} \
--w ${WORKDIR} \
--e MONGO_INITDB_ROOT_USERNAME=${MONGO_USER} \
--e MONGO_INITDB_ROOT_PASSWORD=${MONGO_PASS} \
--e MONGO_INITDB_DATABASE=${DATABASE} \
--d mongo:${MONGOD_VER}
+ -p ${PORT_SERV}:${PORT_CONT} \
+ -v ${INTERNAL_STORAGE}:${EXTERNAL_STORAGE} \
+ --name ${MONGO_NAME} \
+ -w ${WORKDIR} \
+ -e MONGO_INITDB_ROOT_USERNAME=${MONGO_USER} \
+ -e MONGO_INITDB_ROOT_PASSWORD=${MONGO_PASS} \
+ -e MONGO_INITDB_DATABASE=${DATABASE} \
+ -d mongo:${MONGOD_VER}
 
 
 #COPIA EL SCRIPT DE MONGO HACIA EL CONTENEDOR
 docker cp \
-"./set_internal_cont.js" \
-${MONGO_NAME}:${WORKDIR}
+ "./set_internal_cont.js" \
+ ${MONGO_NAME}:${WORKDIR}
 
 #EJECUTA EL ARCHIVO DE INICIALIZACION
 docker exec -it verum-db \
-mongo --host localhost -u ${MONGO_USER} -p ${MONGO_PASS} set_internal_cont.js 
+ mongo --host localhost -u ${MONGO_USER} -p ${MONGO_PASS} set_internal_cont.js 
